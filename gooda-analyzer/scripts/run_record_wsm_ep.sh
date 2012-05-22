f7435528ad9d6ee315a91d7c615a5838a4f82868
Peter Waller <peter.waller@gmail.com>
2012-05-22 11:41:38 +0100

    scripts/*.sh: Find input files in same directory as script

--- gooda-analyzer/scripts/run_record_wsm_ep.sh
+++ gooda-analyzer/scripts/run_record_wsm_ep.sh
@@ -1,4 +1,6 @@
 #/bin/sh
 ulimit -n 32768
-perf record  $(grep -v '^$' wsm_ep_cyc_account.txt | grep -v ^# | sed -e 's/^/--pfm-events /') -a -R  -- $1 $2 $3 $4
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+perf record  $(grep -v '^$' ${SCRIPTPATH}/wsm_ep_cyc_account.txt | grep -v ^# | sed -e 's/^/--pfm-events /') -a -R  -- $1 $2 $3 $4
 
--- gooda-analyzer/scripts/run_stat.sh
+++ gooda-analyzer/scripts/run_stat.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 ulimit -n 32768
-perf stat  $(grep -v '^$' wsm-event1.txt | grep -v ^# | sed -e 's/^/-e /') -a -x @ -- $1 $2 $3 $4
-perf stat  $(grep -v '^$' wsm-event2.txt | grep -v ^# | sed -e 's/^/-e /') -a -x @ -- $1 $2 $3 $4
-perf stat  $(grep -v '^$' wsm-event3.txt | grep -v ^# | sed -e 's/^/-e /') -a -x @ -- $1 $2 $3 $4
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+perf stat  $(grep -v '^$' ${SCRIPTPATH}/wsm-event1.txt | grep -v ^# | sed -e 's/^/-e /') -a -x @ -- $1 $2 $3 $4
+perf stat  $(grep -v '^$' ${SCRIPTPATH}/wsm-event2.txt | grep -v ^# | sed -e 's/^/-e /') -a -x @ -- $1 $2 $3 $4
+perf stat  $(grep -v '^$' ${SCRIPTPATH}/wsm-event3.txt | grep -v ^# | sed -e 's/^/-e /') -a -x @ -- $1 $2 $3 $4
 
